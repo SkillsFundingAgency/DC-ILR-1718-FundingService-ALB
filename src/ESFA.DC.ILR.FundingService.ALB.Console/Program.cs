25b614946ad893c49dff4f6083eed8ae83977b52
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-03-19 12:14:17 +0000

    40925 - Added Stylecop package to console project

--- src/ESFA.DC.ILR.FundingService.ALB.Console/Program.cs
+++ src/ESFA.DC.ILR.FundingService.ALB.Console/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ESFA.DC.ILR.FundingService.ALB.Console
 {
-    static class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            throw (new NotImplementedException());
+            throw new NotImplementedException();
         }
     }
 }
